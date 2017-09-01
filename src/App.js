ec0af22b1a632302ed476e0c02a3f31be3858d31
Carlos Huguet <huguet10@gmail.com>
2017-09-01 18:04:30 +0200

    Create api utils

--- src/App.js
+++ src/App.js
@@ -2,17 +2,39 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import * as api from './utils/api';
+
 class App extends Component {
+  state = {
+    posts: []
+  }
+  listPosts = () => {
+    api.getPosts()
+      .then(posts => {
+        console.log(posts);
+        this.setState(posts);
+      })
+  }
+  componentDidMount() {
+    this.listPosts();
+  }
+  clickButton = (ev) => {
+    ev.preventDefault();
+    api.votePost("8xf0y6ziyjabvozdd253nd", 'upVote')
+    this.listPosts();
+  }
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
+          <p>{this.state.posts}</p>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <button onClick={this.clickButton}>Votar</button>
       </div>
     );
   }
---
+++ src/utils/api.js
@@ -0,0 +1,92 @@
+const BASE_URI = 'http://localhost:5001';
+const TOKEN = '503a657b-1e4c-445b-b745-86341694f8df';
+const headers = {
+  'Accept': 'application/json',
+  'Content-Type': 'application/json',
+  'Authorization': TOKEN
+}
+
+export const getCategories = () => {
+  return fetch(`${BASE_URI}/categories`, {headers})
+  .then(res => res.json());
+}
+
+export const getPostByCategory = (category) => {
+  return fetch(`${BASE_URI}/${category}/posts`, {headers})
+  .then(res => res.json());
+}
+
+export const getPosts = (id = '') => {
+  return fetch(`${BASE_URI}/posts/${id}`, {headers})
+  .then(res => res.json());
+}
+
+export const addPost = ({id, timestamp, title, body, owner, category}) => {
+  return fetch(`${BASE_URI}/posts`, {headers, method: 'POST', body: JSON.stringify({
+    id,
+    timestamp,
+    title,
+    body,
+    owner,
+    category
+  })})
+  .then(res => res.json());
+}
+
+export const votePost = (id = '', typeVote) => {
+  return fetch(`${BASE_URI}/posts/${id}`, {headers, method: 'POST', body: JSON.stringify({
+    option: typeVote
+  })})
+  .then(res => res.json());
+}
+export const editPost = (id = '', {title, body}) => {
+  return fetch(`${BASE_URI}/posts/${id}`, {headers, method: 'PUT', body: JSON.stringify({
+    title,
+    body,
+  })})
+  .then(res => res.json());
+}
+
+export const deletePost = (id = '') => {
+  return fetch(`${BASE_URI}/posts/${id}`, {headers, method: 'DELETE'})
+  .then(res => res.json());
+}
+
+export const getCommentsByPost = (id = '') => {
+  return fetch(`${BASE_URI}/posts/${id}/comments`, {headers})
+  .then(res => res.json());
+}
+
+export const addCommentToPost = ({id, timestamp, body, owner, postId}) => {
+  return fetch(`${BASE_URI}/posts/${id}`, {headers, method: 'POST', body: JSON.stringify({
+    id,
+    timestamp,
+    body,
+    owner,
+    parentId: postId
+  })})
+  .then(res => res.json());
+}
+
+export const getCommentById = (id = '') => {
+  return fetch(`${BASE_URI}/comments/${id}`, {headers})
+  .then(res => res.json());
+}
+
+export const voteComment = (id = '') => {
+  return fetch(`${BASE_URI}/comments/${id}`, {headers, method: 'POST'})
+  .then(res => res.json());
+}
+
+export const editComment = ({ timestamp, id, body }) => {
+  return fetch(`${BASE_URI}/comments/${id}`, {headers, method: 'PUT', body: JSON.stringify({
+    body,
+    timestamp
+  })})
+  .then(res => res.json());
+}
+
+export const deleteComment = (id = '') => {
+  return fetch(`${BASE_URI}/comments/${id}`, {headers, method: 'DELETE'})
+  .then(res => res.json());
+}
